21dd709149f8e5e519e06a66c34cacc67e4412f4
이서준 <seoxuni@iseojun-ui-noteubug.local>
2024-04-25 14:12:20 +0900

    update

---
+++ mid-term/5. arithmeticSequence.py
@@ -0,0 +1,14 @@
+#등차수열의 특정한 항만 더하기 (181931)
+def solution(a, d, included):
+    answer = 0
+    ap = []
+    count = 0
+    while count < len(included):
+        ap.append(a+d*count)
+        count += 1
+    
+    for i in range(len(included)):
+        if included[i]:
+            answer += ap[i]
+        
+    return answer
