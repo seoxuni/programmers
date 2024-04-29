e4cea669acfe6a914b1f65ba36c3ee4bd44cf024
seoxuni <seoxuni@kakao.com>
2024-04-29 13:32:58 +0900

    update

--- mid-term/4. 합성수 찾기.py
+++ mid-term/4. 합성수 찾기.py
@@ -1,16 +1,14 @@
 #합성수 찾기
 def solution(n):
     answer = 0
-    c = 0
     
     for i in range(3, n+1):
-        c = 0
-        for j in range(1, i+1):
+        temp = 1
+        for j in range(1, i):
             if i % j == 0:
-                c += 1
-        if c > 2:
+                temp += 1
+                
+        if temp > 2:
             answer += 1
     
     return answer
-
-print(solution(10))
