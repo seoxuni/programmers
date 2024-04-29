edbeef5efce60985aabfae720749682d91b4bade
seoxuni <seoxuni@kakao.com>
2024-04-29 14:21:30 +0900

    update

--- mid-term/5. 등차수열의 특정한 항만 더하기.py
+++ mid-term/5. 등차수열의 특정한 항만 더하기.py
@@ -1,14 +1,7 @@
 #등차수열의 특정한 항만 더하기
 def solution(a, d, included):
     answer = 0
-    ap = []
-    count = 0
-    while count < len(included):
-        ap.append(a+d*count)
-        count += 1
-    
     for i in range(len(included)):
-        if included[i]:
-            answer += ap[i]
-        
+        if included[i]==True:
+            answer+=a+i*d
     return answer
