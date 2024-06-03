882d67aae347a6ee61baa6c5b43be0a40867fa16
seoxuni <seoxuni@kakao.com>
2024-06-04 08:49:51 +0900

    update

--- final-term/7. 다음 큰 숫자.py
+++ final-term/7. 다음 큰 숫자.py
@@ -1,9 +1,9 @@
 def solution(n):
-    cnt1 = bin(n)[2:].count("1")
+    cnt = bin(n)[2:].count("1")
     
     while True:
         n += 1
-        if cnt1 == bin(n)[2:].count("1"):
+        if cnt == bin(n)[2:].count("1"):
             break
 
     return n
